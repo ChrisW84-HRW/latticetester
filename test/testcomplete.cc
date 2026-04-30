442becfbc31cb62f85f6d3666ddf33597428d2d0
ChrisW84-HRW <83717974+ChrisW84-HRW@users.noreply.github.com>
2026-04-30 16:35:09 +0200

    Added test of IntLattice

--- test/testcomplete.cc
+++ test/testcomplete.cc
@@ -7,7 +7,9 @@ int main() {
     std::cout << "##########################\nTesting Util.h \n########################## \n \n";
     result = system("./testUtil");
     std::cout << "##########################\nTesting Rank1Lattice.h \n########################## \n \n";
-    result = system("./testRank1Lattice");    
+    result = system("./testRank1Lattice");        
+    std::cout << "##########################\nTesting IntLattice.h \n########################## \n \n";
+    result = system("./testIntLattice");    
     std::cout << "##########################\nTesting ReducerStatic.h \n########################## \n \n";
     result = system("./testReducerStatic");    
     std::cout << "##########################\nTesting ReducerBB.h \n########################## \n \n";
